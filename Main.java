ff57fd36685f0aa74d3a7b143e15cc32641b431a
Sergey <saizotov1@gmail.com>
2023-08-03 00:15:49 +0500

    second commit. Создание нового массива с длиной строк меньше или равно трем.

--- Main.java
+++ Main.java
@@ -8,6 +8,14 @@ public class Main {
         for (int i = 0; i < strings.length; i++) {
             strings[i] = scanner.nextLine();
         }
+        String [] newStrings = new String[strings.length];
+        int newIndex;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length() <= 3){
+                newStrings[newIndex] = strings[i];
+                newIndex++;
+            }
+        }
     }
 
 }
